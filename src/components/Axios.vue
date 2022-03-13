80037bc16166df7651533dab590118c51787a6be
shenjy <shenjy24@gmail.com>
2022-03-13 15:18:35 +0800

    axios调用

---
+++ src/components/Axios.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <table class="t">
+      <tr v-for="u in users">
+        <td>{{ u.name }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Axios',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    this.$axios({
+      method: 'post',
+      url: '/user/getUser',
+      data: {}
+    }).then(response => {
+      this.users = response
+      console.log(response, 'success')
+    }).catch(error => {
+      console.log(error, 'error')
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.t {
+  text-align: center;
+}
+</style>
