a22dabe97bc14ba49930be9ab6ce0d6ea1f4da14
shenjy <shenjy24@gmail.com>
2022-03-13 12:11:37 +0800

    重构框架

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hi">
+    {{ msg }}<br>
+    <input type="button" @click="updateMsg('Nice')" value="click"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    updateMsg (msg) {
+      this.msg = msg
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hi {
+  color: red;
+  font-size: 20px;
+}
+</style>
