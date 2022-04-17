f013dbc11bdabd8ca0fb83bf7b1d650433cfce37
shenjy <shenjy24@gmail.com>
2022-04-17 14:43:55 +0800

    css component

---
+++ src/components/css/CssComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CssComponent',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    updateMsg (msg) {
+      this.msg = msg
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  --main-font: Helvetica, Arial, sans-serif;
+}
+
+p {
+  font-family: var(--main-font);
+}
+</style>
